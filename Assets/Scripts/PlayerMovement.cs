84c9d2e88bc14074dad16efa01c45527e09e1f09
Patrick Ramos <patrick.ramos@ciit.edu.ph>
2024-02-17 20:00:10 +0800

    movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //system var
+    private _playerInputs player_Inputs;
+    private Vector2 moveAction;
+
+    //public var
+    private Rigidbody2D rb2d;
+    [SerializeField] float moveSpeed = 10f;
+
+    #region enable _playerInputs
+    private void OnEnable()
     {
-        
+        if (player_Inputs == null)
+        {
+            //enable inputs
+            player_Inputs = new _playerInputs();
+
+            player_Inputs._playerMovements.Movements.performed += i => moveAction = i.ReadValue<Vector2>();
+            player_Inputs._playerMovements.Movements.canceled += i => moveAction = i.ReadValue<Vector2>();
+        }
+
+        player_Inputs.Enable();
+    }
+    private void OnDisable()
+    {
+        if (player_Inputs != null)
+        {
+            player_Inputs._playerMovements.Movements.performed -= i => moveAction = i.ReadValue<Vector2>();
+            player_Inputs._playerMovements.Movements.canceled -= i => moveAction = i.ReadValue<Vector2>();
+        }
+
+        player_Inputs.Disable();
+    }
+    #endregion
+
+    private void Start()
+    {
+        rb2d = GetComponentInChildren<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        playerMovement();
+    }
+
+    #region player movements
+    void playerMovement()
     {
         
+        rb2d.velocity = new Vector2(moveAction.x * moveSpeed, moveAction.y * moveSpeed);
+
+       //clamp nlng naten ung movement ni player para di sya pwede mag close sa pinka righ side -pat
     }
+    #endregion
 }
