ded694a8b31617401808e42fc24fd643151d4322
PatrickRamos <122716128+PatrickMartinRamos@users.noreply.github.com>
2024-03-06 12:17:46 +0800

    health and currency

---
+++ Assets/Scripts/UIScripts/uiDisplayScripts.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class uiDisplayScripts : MonoBehaviour
+{
+    //public var
+    public TextMeshProUGUI _displayHealth;
+    public TextMeshProUGUI _displayCurrency;
+
+    public playerManagerScript _playerManager;
+
+    private void Start()
+    {
+        _playerManager = playerManagerScript._playerManagerInstance;
+    }
+    private void Update()
+    {
+        uiDisplayHealth();
+        uiDisplayCurrency();
+    }
+
+    void uiDisplayHealth()
+    {
+        _displayHealth.text = "Health: " + _playerManager._playerCurrentHealth.ToString();
+    }
+
+    void uiDisplayCurrency()
+    {
+        _displayCurrency.text = "Currency: " + _playerManager.currencyEarned.ToString();
+    }
+}
